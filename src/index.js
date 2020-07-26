7fe9599758260d8b9c72fcc0f2df100c78eef880
THE ROOT <a.benayache@univ-batna2.dz>
2020-07-26 04:07:25 +0200

    footer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 import Header from "components/Header/Header";
 import Footer from "components/Footer/Footer";
 import Components from "views/Components/Components.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
+
+var hist = createBrowserHistory();
 ReactDOM.render(
      <Router history={hist}>
     <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
+
       <Route path="/" component={Components} />
     </Switch>
   </Router>,
