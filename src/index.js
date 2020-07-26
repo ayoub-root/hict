b02d886df2411340382b7a31cd34328563a690e2
THE ROOT <a.benayache@univ-batna2.dz>
2020-07-26 03:43:51 +0200

    footer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import ReactDOM from 'react-dom';
 
 import Header from "components/Header/Header";
 import Footer from "components/Footer/Footer";
-
+import Components from "views/Components/Components.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
 ReactDOM.render(
-    <Header/>
-    <Footer/>,
+     <Router history={hist}>
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/" component={Components} />
+    </Switch>
+  </Router>,
+
+
     document.getElementById('root')
 );
 
